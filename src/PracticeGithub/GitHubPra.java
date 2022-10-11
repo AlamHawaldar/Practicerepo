ccd002a1c220955f00032b9b77a7a52fdfca3271
DELL <DELL@DESKTOP-TD77G35>
2022-10-11 15:45:03 +0530

    second time edit github

--- src/PracticeGithub/GitHubPra.java
+++ src/PracticeGithub/GitHubPra.java
@@ -6,6 +6,8 @@ public static void main(String[] args)
 {
 	System.out.println("Alam");
 	System.out.println("Hawaldar");
+	System.out.println("Velocity 4th june batch");
+	System.out.println("End of testing");
 	
 }
 }
